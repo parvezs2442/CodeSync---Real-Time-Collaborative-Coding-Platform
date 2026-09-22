9cd08f01c6ec9bd4968e5ead569996be448df1e0
Parvez Saifi <parvezs2442@gmail.com>
2026-09-22 16:31:37 +0530

    feat: setup Socket.IO and Yjs collaboration server

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,39 @@
 
 import express from "express"
 import { createServer } from "http"
+
 import { Server } from "socket.io"
 import { YSocketIO } from "y-socket.io/dist/server"
 
 
 const app = express();
 const httpServer = createServer(app) 
+
+//socketio server creation
+const io = new Server(httpServer, {
+    cors: {
+        origin:"*",
+        methods:["GET", "POST"]
+    }
+})
+
+//connecting yjs with socketIO and initializ ing yjs
+const ySocketIO = new YSocketIO(io)
+ySocketIO.initialize()
+
+app.get("/", (req,res) => {
+    res.status(200).json({
+        success:true,
+        message:"Hello "
+    })
+})
+
+app.get("/health", (req,res) => {
+    res.status(200).json({
+        success:true,
+        message:"Ok"
+    })
+})
 
 
 httpServer.listen(3000, () => {
