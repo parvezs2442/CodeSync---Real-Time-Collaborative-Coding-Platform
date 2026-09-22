c17ec4044fd95abfbd63a355e2cf64984e3623ec
Parvez Saifi <parvezs2442@gmail.com>
2026-09-22 17:18:49 +0530

    feat: implement realtime collaborative editor

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,39 @@
-import React from 'react'
+import React, { useRef, useMemo } from 'react'
 import { Editor } from "@monaco-editor/react"
+import { MonacoBinding } from "y-monaco"
+import * as Y from "yjs"
+import { SocketIOProvider } from "y-socket.io"
+
 
 const App = () => {
+
+  //monaco editor reference
+  const editorRef = useRef(null);
+ 
+  //collaborative yjs document
+  const ydoc = useMemo( () => new Y.Doc(), [])
+
+  //shared text inside ydoc
+  const yText = useMemo( () => ydoc.getText("monaco"), [ydoc])
+
+
+  const handleMount = (editor) => {
+    editorRef.current = editor
+    
+    //connects backend with yjs
+    const provider = new SocketIOProvider("http://localhost:3000", "monaco", ydoc, {
+      autoConnect:true,
+    })
+
+    //connects monaco editor with yjs
+    const monacoBinding = new MonacoBinding(
+      yText,
+      editorRef.current.getModel(),
+      new Set([editorRef.current]),
+      provider.awareness
+    )
+  } 
+
   return (
     <main className=' h-screen w-full bg-slate-950 flex gap-4 p-3'>
       <aside className=' h-full bg-slate-200 w-1/4 rounded-md'>
@@ -9,7 +41,12 @@ const App = () => {
       </aside>
 
       <section className=' bg-neutral-800 w-3/4 rounded-lg overflow-hidden'>
-         <Editor height="100%" defaultLanguage="javascript" defaultValue="// some comment" theme='vs-dark' />;
+         <Editor height="100%"
+          defaultLanguage="javascript"
+          defaultValue="// some comment"
+          theme='vs-dark'
+          onMount={handleMount}
+           />;
       </section>
 
     </main>
